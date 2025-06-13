f2da8a21af91474c5226f80bc276c28269ba515e
Chris <53177842+ChristopherMarais@users.noreply.github.com>
2025-06-13 17:06:45 -0400

    Feature/testing and paper (#23)
    * Example notebook with testing data
    
    * features working for OD and OC
    
    * groundingDINO feature extraction

--- src/ibbi/models/__init__.py
+++ src/ibbi/models/__init__.py
@@ -2,5 +2,10 @@
 
 from .classification import yolov10x_bb_classify_model
 from .detection import yolov10x_bb_detect_model
+from .zero_shot_detection import grounding_dino_detect_model
 
-__all__ = ["yolov10x_bb_detect_model", "yolov10x_bb_classify_model"]
+__all__ = [
+    "yolov10x_bb_detect_model",
+    "yolov10x_bb_classify_model",
+    "grounding_dino_detect_model",
+]
